70aab6ec7dd9a6bd124e62fbf8565bf056990358
Ian Y. Choi <ianyrchoi@gmail.com>
2018-12-17 23:40:21 +0900

    소스

---
+++ Hi-Seoul/app/src/main/java/com/hs/hi_seoul/MainActivity.java
@@ -0,0 +1,31 @@
+package com.hs.hi_seoul;
+
+import android.content.Intent;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+    String android_id;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v) {
+        if(v.getId() == R.id.btn_start) {
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+        }
+    }
+}
